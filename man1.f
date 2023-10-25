87f55d20c47ca69345c3fa6764d3a2f9894f2276
Stephen Justin <stevejustin1963@gmail.com>
2023-10-26 07:48:14 +1100

    Update man1.f

--- man1.f
+++ man1.f
@@ -32,3 +32,41 @@
 
 MANDELBROT
 ```
+\\\\\\\\\\\\\\\\\\\\\\\\\\
+pseudocode representation 
+
+PRINT_NEWLINE symbolizes moving to a new line in the output (equivalent to CR in Forth).
+CALL ITERATE() symbolizes invoking the ITERATE routine, which performs one iteration of the Mandelbrot algorithm.
+REPEAT ... UNTIL represents the loop that continues until the specified condition is met (equivalent to BEGIN ... UNTIL in Forth).
+PRINT ASCII CHARACTER (48 + I) prints an ASCII character based on the value of the counter I (equivalent to 48 + EMIT in Forth).
+This should give you a high-level overview of what the Forth code is doing.
+
+\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+FOR Y from -12000 to 12000 step 1000 DO
+    PRINT_NEWLINE
+    
+    FOR X from -39000 to 39000 step 1000 DO
+        Initialize A = 0
+        Initialize B = 0
+        Initialize counter I = 0
+        
+        REPEAT
+            CALL ITERATE()
+            
+            magnitude = A * A + B * B
+            
+            IF magnitude > 40000 OR I > 9 THEN
+                EXIT REPEAT
+            END IF
+            
+        UNTIL magnitude > 40000 OR I > 9
+        
+        PRINT ASCII CHARACTER (48 + I)
+        
+    END FOR (X loop)
+    
+END FOR (Y loop)
+
+End
+\\\\\\\\
